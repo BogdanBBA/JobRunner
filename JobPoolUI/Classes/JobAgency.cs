7bab6aa0540297d776387b9cde197c83885dcf46
BogdanBBA <bogdybba@gmail.com>
2019-01-06 18:43:51 +0200

    Update JobAgency.cs

--- JobPoolUI/Classes/JobAgency.cs
+++ JobPoolUI/Classes/JobAgency.cs
@@ -13,7 +13,8 @@ namespace JobPoolUI.Classes
             new Tuple<int, int>(Const.JOB_HEARTBEAT, Const.FREQUENCY_HEARTBEAT),
             new Tuple<int, int>(Const.JOB_DISK_SPACE, Const.FREQUENCY_DISK_SPACE),
             new Tuple<int, int>(Const.JOB_ANNIVERSARIES, Const.FREQUENCY_ANNIVERSARIES),
-            new Tuple<int, int>(Const.JOB_WEBSTORE_PRODUCTS, Const.FREQUENCY_WEBSTORE_PRODUCTS)
+            new Tuple<int, int>(Const.JOB_WEBSTORE_PRODUCTS, Const.FREQUENCY_WEBSTORE_PRODUCTS),
+            new Tuple<int, int>(Const.JOB_OPEN_WEATHER, Const.FREQUENCY_OPEN_WEATHER)
         };
     }
 }
